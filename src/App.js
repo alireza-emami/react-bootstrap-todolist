6cb44e40089d9938ddfe8c6219ff6a2f088d6054
Alireza Emami <aliemturbo@gmail.com>
2023-05-27 11:41:13 -0700

    cleaning and change the title

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import BasicExample from './react-bootsrap/main';
-import VariationsExample from './react-bootsrap/badge';
-import UncontrolledExample from './react-bootsrap/card';
-import LinkedExample from './react-bootsrap/listgroup';
-import StaticExample from './react-bootsrap/model';
 import Textlist from './react-bootsrap/Textlist';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        {/* <VariationsExample/>
-        <BasicExample/>
-        <UncontrolledExample/>
-        <LinkedExample/>
-        <StaticExample/> */}
-        <Textlist/>
-      {/* </header> */}
+      <Textlist />
     </div>
   );
 }
