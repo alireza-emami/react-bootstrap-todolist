c49c0dbf20f96792e35c7c64d64d737f876c6d85
Alireza Emami <aliemturbo@gmail.com>
2023-05-29 15:14:27 -0700

    fixed components hirarchy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-import Textlist from './react-bootsrap/Textlist';
+import React, { useState, useEffect } from 'react';
+import Textlist from './components/Textlist';
+import TodolistInputs from './components/TodolistInputs';
+import Button from 'react-bootstrap//Button';
 function App() {
+  const localValues = JSON.parse(localStorage.getItem('items') || '[]');
+  const [texts, settext] = useState(localValues);
+  const addtext = (title) => {
+    const newtext = [...texts, { title }];
+    settext(newtext);
+    localStorage.setItem('items', JSON.stringify(newtext))
+  }
+  const clearall = () => {
+    settext([]);
+    localStorage.setItem('items', JSON.stringify([]))
+  }
   return (
-    <div className="App">
-      <Textlist />
+    <div className="app">
+      {/* input */}
+      <TodolistInputs addtext={addtext} />
+      {/* itemes list */}
+      <Textlist settext={settext}  texts={texts}/>
+      {/* actions */}
+      <Button style={{ width: '24rem' }} className='clear' onClick={clearall} variant="outline-danger">clear all</Button>
     </div>
   );
 }
--- src/components/Textlist.js
+++ src/components/Textlist.js
@@ -1,50 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
-import TodolistInputs from './TodolistInputs';
 import CloseButton from 'react-bootstrap/CloseButton';
-import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Form from 'react-bootstrap/Form';
 // import { useState, useEffect } from 'react';
 
-const Textlist = () => {
-    const localValues = JSON.parse(localStorage.getItem('items') || '[]');
-    const [texts, settext] = useState(localValues);
-    const addtext = (title) => {
-        const newtext = [...texts, { title }];
-        settext(newtext);
-        localStorage.setItem('items', JSON.stringify(newtext))
-    }
+const Textlist = ({texts, settext}) => {
     const removeItem = (index) => {
         texts.splice(index, 1);
         const newtext = [...texts];
         settext([...texts]);
         localStorage.setItem('items', JSON.stringify(newtext))
     }
-    const clearall = () => {
-        settext([]);
-        localStorage.setItem('items', JSON.stringify([]))
-    }
     return (
-        <div className='d-flex justify-content-center  ' >
-            <ListGroup as="ol" numbered     >
-                <TodolistInputs addtext={addtext} />
-                {texts.map((item, index) =>
-                    <ListGroup.Item
-                        style={{ width: '24rem' }}
-                        as="li"
-                        className='group1 '
+        <ListGroup as="ol" numbered     >
+            {texts.map((item, index) =>
+                <ListGroup.Item
+                    style={{ width: '24rem' }}
+                    as="li"
+                    className='group1 '
 
-                    >
-                        <label class="form-check-label m"  > {item.title} </label>
-                        <div className='group2'>
-                            <CloseButton onClick={() => removeItem(index)} />
-                        </div>
-                    </ListGroup.Item>)}
-                <Button style={{ width: '24rem' }} className='clear' onClick={clearall} variant="outline-danger">clear all</Button>
-            </ListGroup>
-
-        </div>
+                >
+                    <label class="form-check-label m"  > {item.title} </label>
+                    <div className='group2'>
+                        <CloseButton onClick={() => removeItem(index)} />
+                    </div>
+                </ListGroup.Item>)}
+        </ListGroup>
     );
 };
 
